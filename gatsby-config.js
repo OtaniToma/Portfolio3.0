7606e7345c2c5a8db54713b2af52a768ddc9e8ae
Toma Otani <otani.toma@gmail.com>
2020-06-30 18:47:00 -0700

    add header, import scss

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,39 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: `Toma Otani Portfolio`,
+    description: `This is Toma Otani's portfolio website.`,
+    lang: `ja`,
+    siteUrl: `https://tomaotani.com`,
+    locale: `en_US`,
+    fbappid: `XXXXXXXXXXXXXXXXXXXXX`,
+  },
   /* Your site config here */
   plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Toma Otani Portfolio`,
+        short_name: `Toma Otani Portfolio`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#ffad00`,
+        display: `standalone`,
+        icon: `src/images/icon.png`
+      },
+    },
+    `gatsby-plugin-offline`,
     `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
