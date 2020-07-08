3627f2c7712801f94bbad390d0f43836861f6266
Toma Otani <otani.toma@gmail.com>
2020-07-07 18:01:51 -0700

    add hero component

--- src/pages/works/flightboard.js
+++ src/pages/works/flightboard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../../components/layout'
 import SEO from '../../components/seo'
+import Hero from '../../components/hero'
 
 export default ({ location }) => (
   <Layout>
@@ -9,6 +10,9 @@ export default ({ location }) => (
       pagedesc="Description of Flight Board"
       pagepath={location.pathname}
     />
-    <h1>Flight Board</h1>
+    <Hero
+      title="Flight Board"
+      bgimg="/images/hero/flightboard.jpg"
+    />
   </Layout>
 )
